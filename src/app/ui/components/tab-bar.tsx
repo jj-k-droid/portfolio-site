0499247f06bae04df8a06d44aa2a0d3544fe1db5
jojo <93551276+jj-k-droid@users.noreply.github.com>
2025-06-29 02:28:35 +0300

    added type to children

--- src/app/ui/components/tab-bar.tsx
+++ src/app/ui/components/tab-bar.tsx
@@ -1,4 +1,6 @@
-export default function TabBar({children}) {
+import { PropsWithChildren } from "react";
+
+export default function TabBar({children} : PropsWithChildren) {
     return <div className="flex relative rounded-default outline-4 outline-light-secondary bg-light-browser dark:outline-dark-secondary dark:bg-dark-browser">
         {children}
         <button className="absolute right-2 top-1/4 rounded-full bg-light-accent lg:w-5.5 lg:h-5.5 md:w-4 md:h-4 dark:bg-dark-secondary hover:bg-light-secondary/80
