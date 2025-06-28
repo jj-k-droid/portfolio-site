6e072c0527d6de6f8fa2c2e0261351564d6e0c76
jojo <93551276+jj-k-droid@users.noreply.github.com>
2025-06-29 02:06:53 +0300

    added tab bar component and modified globals.css

---
+++ src/app/ui/components/tab.tsx
@@ -0,0 +1,29 @@
+interface TabProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Tab({ children, className, name, ...rest } : TabProps) {
+    const type = name;
+    if (name === "active") {
+        return <main>
+            <button
+                {...rest}
+                className="flex bg-light-secondary rounded-tab border-4 border-light-secondary pt-tab pl-tab-l pr-tab-r font-heading md:text-xl lg:text-2xl text-light-active-text 
+                dark:bg-dark-secondary dark:text-dark-active-text z-10 after:absolute after:-left-1 box-content dark:border-dark-secondary">
+                    {children}
+                </button>
+        </main>
+    } else if (name === "inactive") {
+        return <main >
+            <button
+                {...rest}
+                className="flex bg-light-inactive-tab rounded-tab pt-tab pl-tab-l pr-tab-r font-heading md:text-xl lg:text-2xl text-light-inactive-text 
+                hover:bg-light-secondary/80 active:text-light-active-text active:bg-light-secondary hover:text-light-active-text dark:bg-dark-inactive-tab
+                dark:text-dark-inactive-text dark:hover:bg-dark-secondary/80 dark:hover:text-dark-inactive-text dark:active:bg-dark-secondary 
+                dark:active:text-dark-active-text box-content border-4 border-light-inactive-tab/0 hover:border-light-secondary/0 
+                active:border-light-secondary dark:border-dark-inactive-tab/0 dark:hover:border-dark-secondary/0 dark:active:border-dark-secondary">
+                    {children}
+                </button>
+        </main>
+    };
+}
