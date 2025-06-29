1e97a3897528c60b3fa33fc856901ce8cc19d945
jojo <93551276+jj-k-droid@users.noreply.github.com>
2025-06-29 19:01:18 +0300

    eslint...

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,7 +2,6 @@
 'use client';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { ThemeProviderProps } from 'next-themes';
-import {useState, useEffect } from 'react';
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
